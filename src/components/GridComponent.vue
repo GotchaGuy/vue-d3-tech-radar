ccd48e0a54deb122164b6ad66ff01fd88ebf9126
GotchaGuy <aljosa100@gmail.com>
2022-02-10 13:42:17 +0100

    activate single item on click - grid

--- src/components/GridComponent.vue
+++ src/components/GridComponent.vue
@@ -7,16 +7,14 @@
           <h3 class="text-base font-bold capitalize">{{ ring.name }}</h3>
         </div>
         <ul class="ring-list">
-          <li v-for="(ringItem, j) in getRingEntries(radarData.entries, i)" @click="toggleModal(ringItem, i, j)"
-              class="ring-item p-2 my-0.5 bg-gray-800 bg-opacity-40 text-base font-light text-white hover:bg-mvp-gray-darker rounded-md flex align-middle" :key="j">
-<!--            <svg class="inline-block mx-2 opacity-100" xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24">-->
-<!--              <path-->
-<!--                  d="M12 3c-4.006 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408-.212-3.951-3.473-7.092-7.479-7.092z"/>-->
-<!--            </svg>-->
+          <li v-for="(ringItem, j) in getRingEntries(entries, i)" @click="toggleModal(ringItem, i, j)"
+              :class="(!ringItem.active) ? 'opacity-60' : ''"
+              class="cursor-pointer ring-item p-2 my-0.5 bg-gray-800 bg-opacity-40 text-base font-light text-white hover:bg-mvp-gray-darker rounded-md flex align-middle"
+              :key="j">
 
-<!--                class="radarData.quadrants[ringItem[j].quadrant].color"-->
+            <!--                class="radarData.quadrants[ringItem[j].quadrant].color"-->
             <div class="inline-block category-color rounded-full p-2 mx-2 my-auto h-2 bg-mvp-green"></div>
-           <span> {{ ringItem.label }} </span>
+            <span> {{ ringItem.label }} </span>
           </li>
         </ul>
       </div>
@@ -44,10 +42,11 @@ export default {
         i: "",
         j: ""
       },
+      entries: [],
     }
   },
   mounted() {
-    // console.log(this.radarData);
+    this.entries = this.radarData.entries
   },
   methods: {
     getRingEntries(entries, ring) {
@@ -63,20 +62,37 @@ export default {
       // when we connect the frontend to the db, add an if that will close modal if click on same item, load diff data on modal if click on diff item
       this.currentItem.i = i;
       this.currentItem.j = j;
-      console.log(this.currentItem.i + " ," + this.currentItem.j);
+      // console.log(this.currentItem.i + " ," + this.currentItem.j);
       if (this.currentItem.j === this.previousItem.j && this.currentItem.i === this.previousItem.i || this.modalDisplay === false) {
-      this.modalDisplay = !this.modalDisplay;
+        this.modalDisplay = !this.modalDisplay;
       }
+      this.activateSingleItem(item, this.currentItem, this.previousItem);
       this.emitter.emit("toggle-modal", {
-        displayed:this.modalDisplay,
+        displayed: this.modalDisplay,
         item: item
       });
       this.previousItem = {
         i: i,
         j: j
       }
-
     },
+    activateSingleItem(item, current, previous) {
+      for (let k = 0; k < this.entries.length; k++) {
+         // instead of .label it will be .id when we connect to db
+        if (this.entries[k].label !== item.label) {
+          if (item.active === false) {
+            item.active = true;
+            this.entries[k].active = false;
+          } else {
+            if (current.j === previous.j && current.i === previous.i) {
+            this.entries[k].active = !this.entries[k].active;
+            } else {
+              this.entries[k].active = false;
+            }
+          }
+        }
+      }
+    }
   },
 
 }
